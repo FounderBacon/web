fd937edd5e2bb82cf675bbe08c283a01727a709f
Lucas <lucas.raffalli17@gmail.com>
2026-04-04 01:07:09 +0200

    feat: add Vercel analytics and speed insights to the project

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,16 @@
 import { AuthGuard } from "@/components/providers/auth-guard"
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return <AuthGuard>{children}</AuthGuard>
+  return (
+    <>
+      <AuthGuard>{children}</AuthGuard>
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
 }
